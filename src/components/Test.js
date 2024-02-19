ee317476f494ed65956ea5b9a5679e4e8aa51946
Chaten Mundle <chetanmundle7875@gmail.com>
2024-02-19 11:37:56 +0530

    kkk

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,4 +1,3 @@
-// Navbar.js
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/infinia.avif";
@@ -14,14 +13,45 @@ const Test = () => {
     return () => clearTimeout(timer);
   }, []);
 
+  const handleScrollTo = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <nav className="navbar">
-      <div>{showLogo && <img className="logo" src={logo} alt="Logo" />}</div>
-      <div className="navbar-buttons">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+    <div>
+      <nav className="navbar">
+        <div>{showLogo && <img className="logo" src={logo} alt="Logo" />}</div>
+        <div className="navbar-buttons">
+          <Link onClick={() => handleScrollTo("first")}>FIRST</Link>
+          <Link onClick={() => handleScrollTo("second")}>SECOND</Link>
+          <Link onClick={() => handleScrollTo("third")}>THIRD</Link>
+          <Link onClick={() => handleScrollTo("fourth")}>FOURTH</Link>
+          <Link onClick={() => handleScrollTo("fifth")}>FIFTH</Link>
+          <Link onClick={() => handleScrollTo("sixth")}>SIXTH</Link>
+        </div>
+      </nav>
+      <div id="first" style={{ height: "50rem", background: "blue"}} >
+        When click on FIRST page start from here
       </div>
-    </nav>
+      <div id="second" style={{ height: "50rem", background: "yellow" }}>
+        When click on SECOND page start from here
+      </div>
+      <div id="third" style={{ height: "50rem", background: "pink" }}>
+        When click on THIRD page start from here
+      </div>
+      <div id="fourth" style={{ height: "50rem", background: "black" }}>
+        When click on FOURTH page start from here
+      </div>
+      <div id="fifth" style={{ height: "50rem", background: "yellow" }}>
+        When click on FIFTH page start from here
+      </div>
+      <div id="sixth" style={{ height: "50rem", background: "blue" }}>
+        When click on SIXED page start from here
+      </div>
+    </div>
   );
 };
 
